64cf9230e5f8fec704296b312d03a582b6d7663a
Chris Hanagan <chris.hanagan@gmail.com>
2019-10-05 22:39:10 -0400

    initial files

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  fltk_xcode
+//
+//  Created by Chris Hanagan on 2/15/18.
+//  Copyright © 2018 Chris Hanagan. All rights reserved.
+//
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+using namespace std;
+//#include "FL/Fl.h"
+
+void but_cb(Fl_Widget* o, void*) {
+  auto* b = (Fl_Button*)o;
+  b->label("Good job");
+
+  b->resize(10, 150, 140, 30);
+  b->redraw();
+}
+
+int main() {
+  // insert code here...
+  Fl_Window win(300, 200, "Testing");
+  win.begin();
+  Fl_Button but(10, 150, 70, 30, "Click me");
+  win.end();
+  but.callback(but_cb);
+  win.show();
+  return Fl::run();
+
+
+}
